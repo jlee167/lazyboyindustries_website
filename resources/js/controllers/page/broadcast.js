210c3a4466bc9694492e57be0e56ecf52f982a2e
Jihoon Lee <anfwkrpdnjs179@gmail.com>
2022-09-24 12:51:42 +0900

    FIX: move vue computed property to methods due to error while initial rendering

--- resources/js/controllers/page/broadcast.js
+++ resources/js/controllers/page/broadcast.js
@@ -115,14 +115,6 @@ window.broadcastApp = new Vue({
         mobileMode: function () {
             return this.viewMode === Stream.ViewModes.MOBILE;
         },
-
-        rtmpMode: function () {
-            return this.stream.videoFormat == Stream.VideoFormats.RTMP;
-        },
-
-        mjpegMode: function () {
-            return this.stream.videoFormat == Stream.VideoFormats.MJPEG;
-        },
     },
 
     methods: {
@@ -137,6 +129,13 @@ window.broadcastApp = new Vue({
         runTestMode: runTestMode,
         joinChatting: joinChatting,
         updateLocation: updateLocation,
+        rtmpMode: function () {
+            return this.stream.videoFormat == Stream.VideoFormats.RTMP;
+        },
+
+        mjpegMode: function () {
+            return this.stream.videoFormat == Stream.VideoFormats.MJPEG;
+        },
     },
 });
 
