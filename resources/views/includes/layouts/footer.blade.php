0a8378076b0558b01d0fb7e03a20aba209ea3825
Jihoon Lee <anfwkrpdnjs179@gmail.com>
2022-09-24 16:12:07 +0900

    refactoring: remove inline css

--- resources/views/includes/layouts/footer.blade.php
+++ resources/views/includes/layouts/footer.blade.php
@@ -1,70 +1,75 @@
-
-
 <article id="footer-container">
-    <div id="footer-contents">
-        <div id="tech-stacks">
-            <p class="footer-column-header">Built With</p>
-            <section class="tech-stack-item">
-                <a class="tech-stack-imgsize">
-                    <img class="tech-stack-imgsize" src="{{asset('/images/php_logo.png')}}" />
-                </a>
-                <p class="tech-stack-label">PHP 8.0</p>
-            </section>
-            <section class="tech-stack-item">
-                <a class="tech-stack-imgsize">
-                    <img class="tech-stack-imgsize" src="{{asset('/images/laravel_logo.png')}}" />
-                </a>
-                <p class="tech-stack-label">Laravel 8</p>
-            </section>
-            <section class="tech-stack-item">
-                <a class="tech-stack-imgsize">
-                    <img class="tech-stack-imgsize" src="{{asset('/images/vue_logo.png')}}" />
-                </a>
-                <p class="tech-stack-label">Vue.js 2.6</p>
-            </section>
-        </div>
-
-        <div id="footer-contact">
-            <p class="footer-column-header">Contact</p>
-            <section class="mb-10px">
-                <img class="footer-contanct-icon" src="/images/icon-building.svg" />
-                <p class="footer-contact-label">LazyBoy Co.Ltd</p>
-            </section>
-            <section class="mb-10px">
-                <img class="footer-contanct-icon" src="/images/icon-mail.svg" />
-                <p class="footer-contact-label">lazyboyindustries.main@gmail.com</p>
-            </section>
-            <section class="mb-10px">
-                <img class="footer-contanct-icon" src="/images/icon-phone.svg" />
-                <p class="footer-contact-label">010-xxxx-xxxx</p>
-            </section>
-
-            <div class="mt-20px">
-                <a href="https://github.com/jlee167" style="width:25px; height:25px;">
-                    <img class="footer-contanct-icon" src="{{asset('/images/GitHub-Mark-Light-32px.png')}}" />
-                </a>
-                <a href="https://github.com/jlee167" style="width:25px; height:25px;">
-                    <img class="footer-contanct-icon" src="{{asset('/images/linkedin.svg')}}">
-                </a>
-                <a href="https://github.com/jlee167" style="width:25px; height:25px;">
-                    <img class="footer-contanct-icon" src="{{asset('/images/google.svg')}}">
-                </a>
-
-            </div>
-        </div>
-
-        <div id="footer-register">
-            <p style="color:white; white-space:nowrap;">Want to participate in my projects?</p>
-            <a id="footer-signBtn" class="btn btn-outline-success" href="/views/register" role="button">
-                Sign up!
-            </a>
-        </div>
-
+  <div id="footer-contents">
+    <div id="tech-stacks">
+      <p class="footer-column-header">Built With</p>
+      <section class="tech-stack-item">
+        <a class="tech-stack-imgsize">
+          <img class="tech-stack-imgsize"
+            src="{{ asset('/images/php_logo.png') }}" />
+        </a>
+        <p class="tech-stack-label">PHP 8.0</p>
+      </section>
+      <section class="tech-stack-item">
+        <a class="tech-stack-imgsize">
+          <img class="tech-stack-imgsize"
+            src="{{ asset('/images/laravel_logo.png') }}" />
+        </a>
+        <p class="tech-stack-label">Laravel 8</p>
+      </section>
+      <section class="tech-stack-item">
+        <a class="tech-stack-imgsize">
+          <img class="tech-stack-imgsize"
+            src="{{ asset('/images/vue_logo.png') }}" />
+        </a>
+        <p class="tech-stack-label">Vue.js 2.6</p>
+      </section>
     </div>
-    <div class="container" style="justify-content:center; padding-top:50px; width:100%">
-        <hr style="height:2px; border-width:0; background-color:white; color:white;">
-        <p id="copyright">
-            Copyright 2022 Lazyboy Industry CO.Limited All Rights Reserved
-        </p>
+
+    <div id="footer-contact">
+      <p class="footer-column-header">Contact</p>
+      <section class="mb-10px">
+        <img class="footer-contanct-icon" src="/images/icon-building.svg" />
+        <p class="footer-contact-label">LazyBoy Co.Ltd</p>
+      </section>
+      <section class="mb-10px">
+        <img class="footer-contanct-icon" src="/images/icon-mail.svg" />
+        <p class="footer-contact-label">lazyboyindustries.main@gmail.com</p>
+      </section>
+      <section class="mb-10px">
+        <img class="footer-contanct-icon" src="/images/icon-phone.svg" />
+        <p class="footer-contact-label">010-xxxx-xxxx</p>
+      </section>
+
+      <div class="mt-20px">
+        <a href="https://github.com/jlee167" class="icon-25px">
+          <img class="footer-contanct-icon"
+            src="{{ asset('/images/GitHub-Mark-Light-32px.png') }}" />
+        </a>
+        <a href="https://github.com/jlee167" class="icon-25px">
+          <img class="footer-contanct-icon"
+            src="{{ asset('/images/linkedin.svg') }}">
+        </a>
+        <a href="https://github.com/jlee167" class="icon-25px">
+          <img class="footer-contanct-icon"
+            src="{{ asset('/images/google.svg') }}">
+        </a>
+
+      </div>
     </div>
+
+    <div id="footer-register">
+      <p id="signupPrompt">Want to participate in my projects?</p>
+      <a id="footer-signBtn" class="btn btn-outline-success"
+        href="/views/register" role="button">
+        Sign up!
+      </a>
+    </div>
+
+  </div>
+  <div id="copyrightContainer" class="container">
+    <hr>
+    <p id="copyright">
+      Copyright 2022 Lazyboy Industry CO.Limited All Rights Reserved
+    </p>
+  </div>
 </article>
