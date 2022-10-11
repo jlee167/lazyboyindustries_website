5b43b74b73f5cf5654052027afc3045b507066e5
Jihoon Lee <anfwkrpdnjs179@gmail.com>
2022-10-11 20:53:30 +0900

    [FIX] fix wrong namespace import

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\StreamController;
+use App\Http\Controllers\SupportController;
 
 /*
 |--------------------------------------------------------------------------
