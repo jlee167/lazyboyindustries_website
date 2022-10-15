6c39a6ef05a8435acee967af0804ce6c199a4022
Jihoon Lee <anfwkrpdnjs179@gmail.com>
2022-10-15 11:02:01 +0900

    [Change] Changing support page appearance

--- resources/js/components/QnaDropdown.vue
+++ resources/js/components/QnaDropdown.vue
@@ -98,7 +98,7 @@ export default {
   width: 100%;
   padding-bottom: 0.7rem;
   padding-left: 48px;
-  margin-bottom: 1rem;
+  margin-top: 1rem;
   color: #4e5968;
   font-family: "Nunito Sans", sans-serif !important;
   font-size: 18px;
