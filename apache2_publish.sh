3543a6fbe8a1bf6d427d83508619d272d57924b3
Jihoon Lee <anfwkrpdnjs179@gmail.com>
2022-10-15 15:19:09 +0900

    [FIX] Fix wrong script code for storing images before update

--- apache2_publish.sh
+++ apache2_publish.sh
@@ -14,13 +14,15 @@ npm install --loglevel=error
 npm run dev --loglevel=error
 #npm run production --loglevel=error
 
+#Save existing profile images
+sudo cp -r -u ${DEST}/public/images/* ./public/images
+
 sudo rm -r ${DEST}/*
 sudo cp -r ${SRC} ${DEST}
 sudo cp ~/.env ${DEST}
 sudo cp ~/.env ./
 
-#Save existing profile images
-sudo cp -r -u ${DEST}/public/images/* ./public/images
+
 
 #sudo cp -r -f ./vendor ${DEST}
 #sudo cp -r -f ./streams ${DEST}
