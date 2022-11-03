374503930f7c728ddfa44807ca5805f457742bac
Jihoon Lee <anfwkrpdnjs179@gmail.com>
2022-11-03 20:57:33 +0900

    [Refactoring] Change vue component name

--- resources/js/app.js
+++ resources/js/app.js
@@ -31,7 +31,7 @@ window.Vue = require('vue');
 
 Vue.component('input-tag', () => import('vue-input-tag'));
 Vue.component('star-rating', () => import('vue-star-rating'));
-Vue.component('notification', () => import('vue-notification'));
+Vue.component('notifications', () => import('vue-notification'));
 
 Vue.component('pulse-loader', () => import('vue-spinner/src/PulseLoader'));
 
