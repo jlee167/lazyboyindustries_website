74235dc967bb75a0164a810f66c5a677059f54bc
Jihoon Lee <anfwkrpdnjs179@gmail.com>
2022-11-12 14:09:32 +0900

    [TEST] Debug logging routine added to forgot-password route

--- routes/web.php
+++ routes/web.php
@@ -165,6 +165,8 @@ Route::post('/forgot-password', function (Request $request) {
         $request->only('email')
     );
 
+    Log::info($status);
+
     return $status === Password::RESET_LINK_SENT
                 ? back()->with(['status' => __($status)])
                 : back()->withErrors(['email' => __($status)]);
