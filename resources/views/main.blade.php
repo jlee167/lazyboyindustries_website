b9099dbc71a29bef4df3bafc378ff3578cc724c5
Jihoon Lee <anfwkrpdnjs179@gmail.com>
2023-01-30 21:29:32 +0900

    [FIX] wrong file name

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -35,7 +35,7 @@
         </video>
         <div id="logoView" class="scrollable-page">
           <img class="img-logo"
-            src="{{ asset('/images/logo_final_7.jpg') }}">
+            src="{{ asset('/images/logo_final_7.png') }}">
           <h1 class="front-label"> Lazyboy Industries</h1>
           <scroll-arrow direction="down" :motion-enabled="true" class="mt-5">
           </scroll-arrow>
