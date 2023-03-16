8beb84aed0cfc5944d2f13dd10c54877189dcc66
Jihoon Lee <anfwkrpdnjs179@gmail.com>
2023-03-16 22:01:24 +0900

    [UI] Limit maximum particles responsively

--- resources/js/controllers/page/main.js
+++ resources/js/controllers/page/main.js
@@ -45,6 +45,33 @@ function onLoad() {
         selector: '.bg-particle',
         color: '#ffffff',
         connectParticles: true,
+        maxParticles:
+            300
+        ,
+
+        // options for breakpoints
+        responsive: [
+            {
+                breakpoint: 768,
+                options: {
+                    maxParticles: 200,
+                    color: '#48F2E3',
+                    connectParticles: false
+                }
+            }, {
+                breakpoint: 425,
+                options: {
+                    maxParticles: 100,
+                    connectParticles: true
+                }
+            }, {
+                breakpoint:320,
+                options: {
+                    maxParticles:
+                        30
+                }
+            }
+        ],
     });
 
     this.observeSkillView();
