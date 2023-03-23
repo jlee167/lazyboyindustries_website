e332462b790f9b3202d1cdb2f15935a9ed204274
Jihoon Lee <anfwkrpdnjs179@gmail.com>
2023-03-23 21:09:39 +0900

    [UI] Changed FAQ texts

--- resources/views/includes/imports/strings/qna_strings.blade.php
+++ resources/views/includes/imports/strings/qna_strings.blade.php
@@ -6,22 +6,21 @@
     window.strings.FAQ_CONTENTS = [
         {
             question: "What does 'broadcast' page do?",
-            answer: "It is a demo page for live broadcast of my blackbox projects. \n
-                    Currently, it plays a public video stream and GPS data from my \n
-                    server for demo purpose. I cannot stream video myself due to \n
-                    server fees"
+            answer: "It is a demo page for live broadcast of my blackbox projects. \n"
+                    + "Currently, it plays a public video stream and GPS data from my \n"
+                    + "server for demo purpose. I cannot stream video myself due to \n"
+                    + "server fees"
         },
         {
             question: "Are you open for work?",
-            answer: "I am currenlty seeking a job in hardware and software engineering. \n
-                    I hope to work in areas close to video processing or robotics. \n
-                    I am also ready to explore other areas including medical devices \n
-                    or smart farms.",
+            answer: "I am currenlty seeking a job in hardware and software engineering. \n"
+                    + "I hope to work in areas close to video processing or robotics. \n"
+                    + "I am also ready to explore other areas including medical devices \n or smart farms.",
         },
         {
             question: "How can I contact you?",
-            answer: "An please email to lazyboyindustries.main@gmail.com. I should be \n
-                  able to respond in a few hours at most."
+            answer: "An please email to lazyboyindustries.main@gmail.com. I should be \n"
+                  + "able to respond in a few hours at most.",
         },
     ];
 </script>
