413e4978530a648a51630f5a4f4fe08b581d2b44
Jihoon Lee <anfwkrpdnjs179@gmail.com>
2023-05-14 19:48:17 +0900

    [FIX] fixing particle color

--- resources/js/controllers/page/main.js
+++ resources/js/controllers/page/main.js
@@ -43,7 +43,7 @@ function onLoad() {
     this.loaded = true;
     this.particles = Particles.init({
         selector: '.bg-particle',
-        color: 'white',
+        color: '#ffffff',
         connectParticles: true,
         maxParticles:
             100
